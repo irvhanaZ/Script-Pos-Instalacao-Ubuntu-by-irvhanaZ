6de1c4640515ca3c0c6b98a3da82649101b2ac84
irvhanaZ <kanaazuka@hotmail.com>
2025-04-26 01:49:33 -0300

    modificacoesv7

--- pos_ubuntu.sh
+++ pos_ubuntu.sh
@@ -15,8 +15,10 @@ echo "Script de pós instalação para ubuntu by:"
                                                                            
 echo "Preparando o sistema..."
 
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt list --upgradable -y
+sudo apt upgrade -y
 sudo ufw enable
+sudo apt install --fix-broken install -y
 
 echo "baixando utilitários..."
 
@@ -25,11 +27,11 @@ sudo apt install gnome-software -y
 sudo apt install gnome-shell-extension-gsconnect -y
 sudo apt install wget -y
 sudo apt-get install libxss1 libappindicator1 libindicator7 -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome*.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -y
+sudo apt install ./google-chrome*.deb -y
 wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb 
-sudo dpkg -i vscode.deb 
-sudo apt install -f
+sudo apt install ./vscode.deb -y
+sudo apt install -f -y
 
 echo "instalando o gnome vanilla..."
 
@@ -37,17 +39,26 @@ sudo apt remove ubuntu-session -y
 sudo apt purge ubuntu-desktop -y
 sudo apt install gnome-session -y
 sudo apt install vanilla-gnome-desktop && sudo apt install vanilla-gnome-default-settings -y
-
+sudo apt install -f 
 
 echo "Desinstalando Snaps e a dicionando suporte a flatpak..."
 sudo apt purge snapd -y
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo apt install -f
 
 echo "Preparando o sistema para jogos..."
-sudo apt install wine-stable
+sudo apt install wine-stable -y
+sudo flatpak install flathub com.valvesoftware.Steam
+wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.16.1/Heroic-2.16.1-linux-amd64.deb
+sudo dpkg -i Heroic-2.16.1-linux-amd64.deb
+sudo flatpak install flathub net.davidotek.pupgui2
+wget https://github.com/lutris/lutris/releases/download/v0.5.18/lutris_0.5.18_all.deb
+sudo apt install ./lutris*.deb
+sudo apt install -f
 
-
+echo "Finalizando pos instalacao e reiniciando sistema..."
 sudo apt autoremove --purge -y
-sudo apt reboot -0
+sudo clean && sudo autoclean
+sudo systemctl reboot
