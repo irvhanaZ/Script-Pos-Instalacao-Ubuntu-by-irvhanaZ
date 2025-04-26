2bf3cd1cf2926dd9bb1ca490c50a09184cc70f24
fzvzvcz <kanaazuka@hotmail.com>
2025-04-26 03:49:54 -0300

    modificacoesv13

--- pos_ubuntu.sh
+++ pos_ubuntu.sh
@@ -15,10 +15,8 @@ echo "Script de pós instalação para ubuntu by:"
                                                                            
 echo "Preparando o sistema..."
 
-sudo apt update -y && sudo apt list --upgradable -y
-sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 sudo ufw enable
-sudo apt install --fix-broken install -y
 
 echo "baixando utilitários..."
 
@@ -26,12 +24,11 @@ sudo apt install git -y
 sudo apt install gnome-software -y
 sudo apt install gnome-shell-extension-gsconnect -y
 sudo apt install wget -y
-sudo apt-get install libxss1 libappindicator1 libindicator7 -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -y
+#sudo apt-get install libxss1 libappindicator1 libindicator7 -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo apt install ./google-chrome*.deb -y
-wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb 
+wget "https://go.microsoft.com/fwlink/?LinkID=760868" 
 sudo apt install ./vscode.deb -y
-sudo apt install --fix-broken install -y
 
 echo "instalando o gnome vanilla..."
 
@@ -39,7 +36,6 @@ sudo apt remove ubuntu-session -y
 sudo apt purge ubuntu-desktop -y
 sudo apt install gnome-session -y
 sudo apt install vanilla-gnome-desktop && sudo apt install vanilla-gnome-default-settings -y
-sudo apt install --fix-broken install -y
 
 echo "Desinstalando Snaps e a dicionando suporte a flatpak..."
 
@@ -47,7 +43,6 @@ sudo apt purge snapd -y
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo apt install --fix-broken install -y
 
 echo "Preparando o sistema para jogos..."
 sudo apt install wine-stable -y
@@ -55,9 +50,7 @@ sudo flatpak install flathub com.valvesoftware.Steam -y
 sudo flatpak install flathub com.heroicgameslauncher.hgl -y
 sudo flatpak install flathub net.davidotek.pupgui2 -y
 sudo flatpak install flathub net.lutris.Lutris -y
-sudo apt install --fix-broken install -y
 
 echo "Finalizando pos instalacao e reiniciando sistema..."
 sudo apt autoremove --purge -y
 sudo clean && sudo autoclean -y
-sudo systemctl reboot
