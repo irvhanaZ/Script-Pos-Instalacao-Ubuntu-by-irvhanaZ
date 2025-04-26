bff0d96e3a45cd008acc910de6d843dc6c819661
irvhanaZ <kanaazuka@hotmail.com>
2025-04-25 22:57:37 -0300

    modificações v2

--- pos_ubuntu.sh
+++ pos_ubuntu.sh
@@ -2,13 +2,13 @@
 
 echo "Script de pós instalação para ubuntu by:"
 
- echo   "$$\                      $$\                                     $$$$$$$$\ " 
- echo   " \__|                     $$ |                                    \____$$  | "
- echo   " $$\  $$$$$$\  $$\    $$\ $$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\      $$  /  "
- echo   " $$ |$$  __$$\ \$$\  $$  |$$  __$$\  \____$$\ $$  __$$\  \____$$\    $$  /   "
- echo   " $$ |$$ |  \__| \$$\$$  / $$ |  $$ | $$$$$$$ |$$ |  $$ | $$$$$$$ |  $$  /    "
- echo   " $$ |$$ |        \$$$  /  $$ |  $$ |$$  __$$ |$$ |  $$ |$$  __$$ | $$  /     "
- echo   " $$ |$$ |         \$  /   $$ |  $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$ |$$$$$$$$\  "
+ echo   "##\                      ##\                                     ########\ " 
+ echo   " \__|                     ## |                                    \____##  | "
+ echo   " ##\  ######\  ##\    ##\ #######\   ######\  #######\   ######\      ##  /  "
+ echo   " ## |##  __##\ \##\  ##  |##  __##\  \____##\ ##  __##\  \____##\    ##  /   "
+ echo   " ## |## |  \__| \##\##  / ## |  ## | ####### |## |  ## | ####### |  ##  /    "
+ echo   " ## |## |        \###  /  ## |  ## |##  __## |## |  ## |##  __## | ##  /     "
+ echo   " ## |## |         \#  /   ## |  ## |\####### |## |  ## |\####### |########\  "
  echo   " \__|\__|          \_/    \__|  \__| \_______|\__|  \__| \_______|\________| "
                                                                            
                                                                            
@@ -23,9 +23,27 @@ sudo apt install git -y
 sudo apt install code -y
 sudo apt install gnome-software -y
 sudo apt install gnome-shell-extension-gsconnect -y
+sudo apt install wget -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb.
+dpkg -i ~/Downloads/google-chrome-stable*.deb.
+wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb -y
+sudo dpkg -i vscode.deb -y
+sudo apt install -f
 
 echo "Desinstalando Snaps e a dicionando suporte a flatpak..."
 sudo apt purge snapd -y
+sed -i "\$a# Para evitar que os pacotes do repositório despoletem a instalação do snap," /etc/apt/preferences.d/nosnap.pref
+sed -i "2i # este ficheiro proíbe que o snapd seja instalado pelo APT." /etc/apt/preferences.d/nosnap.pref
+sed -i "3i Package: snapd" /etc/apt/preferences.d/nosnap.pref
+sed -i "4i Pin: release a=*" /etc/apt/preferences.d/nosnap.pref
+sed -i "Pin-Priority: -10" /etc/apt/preferences.d/nosnap.pref
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
-$ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo "instalando o gnome vanilla..."
+
+sudo apt install gnome-session -y
+sudo apt install vanilla-gnome-desktop vanilla-gnome-default-settings -y
+sudo apt purge ubuntu-desktop-gnome-shell-extension-ubuntu-dock
+sudo aot autoremove --purge
